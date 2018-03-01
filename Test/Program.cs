b8894c37006c91b339cacd16baecefc12c379766
Carlos Francisco Rodríguez Aguilar <crodriguez20@ucol.mx>
2018-02-28 23:27:28 -0600

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if (1L == 1f) System.Console.WriteLine("EQUALS");
+
+        }
+    }
+}
